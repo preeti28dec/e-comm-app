f1f8046e03ebc43f83e8cd8cfc3474a1ea47f83f
preeti28dec <preetithakur28dec2000@gmail.com>
2022-08-09 16:05:08 +0530

    done

--- pages/components/Footer.jsx
+++ pages/components/Footer.jsx
@@ -10,7 +10,7 @@ function Footer() {
                             <div> <svg className="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http:www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" /></svg></div>
                             <div className='footer_text'>E-Comm</div>
                         </div>
-                        <div className='footer_second_section_last_text'>Lorem Ipsum is simply dummy text of the<br /> printing and typesetting industry. Lorem <br />Ipsum has been the industry's standard <br />dummy text ever.Since the 1500s, when <br />an unknown printer.</div>
+                        <div className='footer_second_section_last_text'>Lorem Ipsum is simply dummy text of the<br /> printing and typesetting industry. Lorem <br />Ipsum has been the industry standard <br />dummy text ever.Since the 1500s, when <br />an unknown printer.</div>
                     </div>
 
                     <div className=''> <div className='footer_follow_text my-4'>Follow Us</div>
