5b4c87f6f450def28f1acfbf9110df6867caa04c
preeti28dec <preetithakur28dec2000@gmail.com>
2022-11-28 17:58:25 +0530

    add login page

--- pages/singup.jsx
+++ pages/singup.jsx
@@ -56,7 +56,7 @@ const Signup = () => {
         </div>
         <div className="mt-6 ">
           <Link className="text-blue-500 hover:underline ml-6" href="/login">
-            Login page
+           <a> Login page</a>
           </Link>
         </div>
       </div>
